48772592d47594f25d6440a69ac445b44e9a021f
Modesto San Juan <modestosja@gmail.com>
2017-01-04 23:49:54 +0100

    All nuget references added

---
+++ PollyShould.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace PollyPlayground
+{
+    public class PollyShould
+    {
+        [Fact]
+        public void execute_empty_test()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
